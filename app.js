3ff7127b540d45c8a2e5f0e836302e17df83e366
ignatiusomeje <mrexcel153@gmail.com>
2023-09-10 17:08:31 +0100

    add github file url and github repo url. remove the console.log in app.js

--- app.js
+++ app.js
@@ -12,15 +12,14 @@ App.get("/api", (req, res, next) => {
       "Saturday",
       "Sunday",
     ];
-    console.log(req.query);
     const { slack_name, track } = req.query;
     return res.json({
       slack_name,
       current_day: day[new Date().getUTCDay()],
       utc_time: new Date().toISOString(),
       track,
-      github_file_url: "",
-      github_repo_url: "",
+      github_file_url: "https://github.com/ignatiusomeje/hngxAssignment/blob/main/app.js",
+      github_repo_url: "https://github.com/ignatiusomeje/hngxAssignment",
       status_code: 200,
     });
   } catch (error) {
